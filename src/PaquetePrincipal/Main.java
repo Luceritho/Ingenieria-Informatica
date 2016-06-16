9b3e9fbb4653709b61209b898e9a220a709bd455
lynnn <lynnn@192.168.1.14>
2016-06-16 13:06:20 -0300

    Ultima prueba

--- src/PaquetePrincipal/Main.java
+++ src/PaquetePrincipal/Main.java
@@ -17,6 +17,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ingenieria Prueba");
         System.out.println("Ingenieria Prueba del MASTER !!");
+        System.err.println("Pollos !!");
     }
     
 }
